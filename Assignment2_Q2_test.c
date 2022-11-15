b657b265b08e5193453d439c77b683b01b3802a7
Shobhit21287 <102307364+Shobhit21287@users.noreply.github.com>
2022-11-15 17:34:44 +0530

    Add files via upload

--- Assignment2_Q2_test.c
+++ Assignment2_Q2_test.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 void main(){
-    int arr = {{1,2,3},{4,5,6},{7,8,9}};
+    double arr = {{1.23,2.34,3.45},{4.56,5.67,6.78},{7.9012,8.2345,9.5678}};
     
-    int **arr2;
+    double arr2[3][3];
 
-    twodcopy(arr,arr2,3,3);
+    syscall(twodcopy,arr,arr2,3,3);
 
     for(int i = 0 ; i < 3 ; i++){
         for(int j = 0 ; j < 3 ;j++){
-            printf("%d ",arr2[i][j]);
+            printf("%f ",arr2[i][j]);
         }
         printf("\n");
     }
--- Assignment2_Syscall.c
+++ Assignment2_Syscall.c
@@ -1,22 +1,18 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE5(twodcopy, int* src, int* dest, int rows, int columns,int row_num, int col_num){
-    int *temp1 = dest;
-    int *temp2 = src;
+SYSCALL_DEFINE4(twodcopy, int* src, int* dest,int row_num, int col_num){
     
-    int buffer[col_num];
+    int elements = row_num*col_num;
 
-    for(int i = 0 ; i < row_num ; i++){
-        if(__copy_from_user(buffer, temp2, sizeof(int)*col_num))
-            return -EFAULT;
-        if(__copy_to_user(temp1, buffer, sizeof(int)*col_num))
-            return -EFAULT;
-        temp1 += sizeof(int)*col_num;
-        temp2 += sizeof(int)*col_num;
-    }
-    
-    free(temp1);
-    free(temp2);
+    int buffer[elements];
+
+    int size = sizeof(long int)*elements;
+
+    if(__copy_from_user(buffer, src, size))
+        return -EFAULT;
+    if(__copy_to_user(dest, buffer, sizez))
+        return -EFAULT;
+
     return 0;
 }
